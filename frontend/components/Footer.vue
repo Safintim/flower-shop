f1ad5708332947e28cd95744f136f450282ad317
Timur Safin <timur@getgain.ru>
2020-05-01 02:33:25 +0300

    Footer update

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,15 +1,19 @@
 <template>
-  <footer class="p-5">
+  <footer class="px-5 pt-5 pb-4">
     <b-container>
       <b-row class="justify-content-center">
         <b-col class="logo text-center" cols="12" sm="12" md="3">
-          <b-img-lazy src="~/assets/images/logo_footer.png" alt="logo_footer.png" class="img-fluid" />
+          <b-img-lazy
+            src="~/assets/images/logo_footer.png"
+            alt="logo_footer.png"
+            class="img-fluid"
+          />
           <div class="copyright">© Служба доставки цветов Flow-service, 2019</div>
         </b-col>
-        <b-col class="menu text-right" cols="12" sm="12" md="3">
+        <b-col class="menu text-left" cols="12" sm="12" md="1" offset-md="2">
           <div v-for="item of 5" :key="item">Цветы</div>
         </b-col>
-        <b-col class="nav-footer text-left" right cols="12" sm="12" md="3">
+        <b-col class="nav-footer text-left" cols="12" sm="12" md="3">
           <div>О магазине</div>
           <div>Доставка</div>
           <div>Контакты</div>
