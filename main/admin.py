5bfc4ae829ff431d776ecd1b9cbfd028574377b9
Safintim <timurtlt96@mail.ru>
2021-03-03 16:35:52 +0300

    Add models for bouquets

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 
 from main import models
 
+
+class BouquetFlowerMiddleInline(admin.TabularInline):
+    model = models.BouquetFlowerMiddle
+    extra = 0
+
+
+class BouquetFlowerSmallInline(admin.TabularInline):
+    model = models.BouquetFlowerSmall
+    extra = 0
+
+
+class BouquetFlowerBigInline(admin.TabularInline):
+    model = models.BouquetFlowerBig
+    extra = 0
+
+
+class BouquetAdmin(admin.ModelAdmin):
+    inlines = (BouquetFlowerMiddleInline, BouquetFlowerSmallInline, BouquetFlowerBigInline)
+
+
 admin.site.register(models.Reason)
 admin.site.register(models.Category)
 admin.site.register(models.Color)
 admin.site.register(models.Product)
+admin.site.register(models.Flower)
+admin.site.register(models.Bouquet, BouquetAdmin)
+# admin.site.register(models.BouquetFlower)
--- main/models.py
+++ main/models.py
@@ -38,13 +38,13 @@ class Color(models.Model):
 class Product(models.Model):
     title = models.CharField('Название', max_length=200)
     slug = models.SlugField('Слаг', unique=True)
-    price = models.DecimalField('Цена', max_digits=9, decimal_places=2)
-    image = models.ImageField('Изображение')
+    price = models.DecimalField('Цена', max_digits=9, decimal_places=2, default=0, blank=True)
+    image = models.ImageField('Изображение', blank=True)
     is_active = models.BooleanField('Активный', default=False)
-    discount = models.FloatField('Скидка', default=0)
+    discount = models.FloatField('Скидка', default=0, blank=True)
     categories = models.ManyToManyField(Category, verbose_name='Категории')
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, blank=False, null=True, verbose_name='Цвет')
-    reasons = models.ManyToManyField(Reason, blank=False, verbose_name='Поводы')
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Цвет')
+    reasons = models.ManyToManyField(Reason, blank=True, verbose_name='Поводы')
 
     class Meta:
         verbose_name = 'Товар'
@@ -53,3 +53,75 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
+class Flower(models.Model):
+    title = models.CharField('Название', max_length=100)
+    price = models.DecimalField('Цена', max_digits=9, decimal_places=2)
+    is_add_filter = models.BooleanField('Добавить в фильтр', default=True)
+
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Bouquet(Product):
+    small_size = models.ManyToManyField(
+        Flower,
+        related_name='sm_size',
+        through='main.BouquetFlowerSmall',
+        verbose_name='Цветы',
+        blank=False
+    )
+    middle_size = models.ManyToManyField(
+        Flower,
+        related_name='md_size',
+        through='main.BouquetFlowerMiddle',
+        verbose_name='Цветы',
+        blank=False
+    )
+    big_size = models.ManyToManyField(
+        Flower,
+        related_name='bg_size',
+        through='main.BouquetFlowerBig',
+        verbose_name='Цветы',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Букет'
+        verbose_name_plural = 'Букеты'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class BouquetFlowerMiddle(models.Model):
+    count = models.PositiveIntegerField('Количество', default=0)
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE, verbose_name='Цветок', )
+    bouquet = models.ForeignKey(Bouquet, on_delete=models.CASCADE, verbose_name='Букет', )
+
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы в средних букетах'
+
+    def __str__(self):
+        return f'{self.flower} - {self.bouquet}'
+
+
+class BouquetFlowerSmall(BouquetFlowerMiddle):
+    class Meta:
+        proxy = True
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы в маленьких букетах'
+
+
+class BouquetFlowerBig(BouquetFlowerMiddle):
+    class Meta:
+        proxy = True
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы в больших букетах'
