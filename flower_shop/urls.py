3f7ad74c7027767324c3a07b35f943e21dc8f441
Safintim <timurtlt96@mail.ru>
2021-03-04 14:42:49 +0300

    Add base template

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
+
+
+def index(request):
+    return render(request, 'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
