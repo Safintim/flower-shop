95257b889a6b584de25ccf4459c481eb59ef2163
Safintim <timurtlt96@mail.ru>
2021-03-18 17:51:37 +0300

    Add errors page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def handler403(request, exception):
+    return render(request, '403.html', status=403)
